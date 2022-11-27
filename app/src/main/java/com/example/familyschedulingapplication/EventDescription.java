8ff525fe7fb6b12f9ccbebc51cf0cc9cce7ccc12
Manroop22 <77290018+Manroop22@users.noreply.github.com>
2022-11-27 01:15:48 -0800

    Here the event description page design has been added. NOTE: The powermenus for the kebab menus have not been added at the moment. Also, the calendar to take the date input has also not been added.

---
+++ app/src/main/java/com/example/familyschedulingapplication/EventDescription.java
@@ -0,0 +1,14 @@
+package com.example.familyschedulingapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventDescription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_description);
+    }
+}
