e40af33953e3710bde262dc22c3533449f399715
Cleman25 <clementan25@gmail.com>
2022-11-26 21:54:57 -0800

    Removed unused Items. Theming changes

--- app/src/main/java/com/example/familyschedulingapplication/ModalBottomSheet.java
+++ app/src/main/java/com/example/familyschedulingapplication/ModalBottomSheet.java
@@ -6,21 +6,14 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 
-import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.TextView;
-
-import com.example.familyschedulingapplication.databinding.FragmentModalBottomSheetListDialogItemBinding;
-import com.example.familyschedulingapplication.databinding.FragmentModalBottomSheetListDialogBinding;
 
 /**
  * <p>A fragment that shows a list of items as a modal bottom sheet.</p>
@@ -34,7 +27,6 @@ public class ModalBottomSheet extends BottomSheetDialogFragment {
     public static final String TAG = "ModalBottomSheet";
     // TODO: Customize parameter argument names
     private static final String ARG_ITEM_COUNT = "item_count";
-//    private FragmentModalBottomSheetListDialogBinding binding;
 
     // TODO: Customize parameters
     public static ModalBottomSheet newInstance(int itemCount) {
@@ -49,8 +41,7 @@ public class ModalBottomSheet extends BottomSheetDialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_modal_bottom_sheet_list_dialog, container, false);
-//        binding = FragmentModalBottomSheetListDialogBinding.inflate(inflater, container, false);
+        View view = inflater.inflate(R.layout.fragment_modal_bottom_sheet_dialog, container, false);
         ImageButton homeBtn = view.findViewById(R.id.homeBtn);
         ImageButton calendarBtn = view.findViewById(R.id.calendarBtn);
         ImageButton billsBtn = view.findViewById(R.id.billsBtn);
@@ -59,7 +50,6 @@ public class ModalBottomSheet extends BottomSheetDialogFragment {
         ImageButton logoutBtn = view.findViewById(R.id.logoutBtn);
         homeBtn.setOnClickListener(v -> goTo(MainActivity.class));
         logoutBtn.setOnClickListener(v -> MainActivity.signOut());
-//        return binding.getRoot();
 
         return view;
     }
@@ -68,14 +58,4 @@ public class ModalBottomSheet extends BottomSheetDialogFragment {
         Intent intent = new Intent(getActivity(), activity);
         startActivity(intent);
     }
-
-//    @Override
-//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-//    }
-
-//    @Override
-//    public void onDestroyView() {
-//        super.onDestroyView();
-//        binding = null;
-//    }
 }
