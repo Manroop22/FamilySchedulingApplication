5408f51ed1fcaf45437980b4ce462c6a845c6402
Manroop22 <77290018+Manroop22@users.noreply.github.com>
2022-12-01 14:17:23 -0800

    headers added to the recycler views

--- app/src/main/java/com/example/familyschedulingapplication/MessageBoard.java
+++ app/src/main/java/com/example/familyschedulingapplication/MessageBoard.java
@@ -1,6 +1,8 @@
 package com.example.familyschedulingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -8,8 +10,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class MessageBoard extends AppCompatActivity {
     private static final String TAG = "MessageBoard";
+    private ArrayList<Event> eventList = new ArrayList<>();
+    private EventAdapter eventAdapter;
     RecyclerView eventRecycler;
     RecyclerView billRecycler;
     RecyclerView msgRecycler;
@@ -26,6 +33,12 @@ public class MessageBoard extends AppCompatActivity {
         billRecycler=findViewById(R.id.billsRecyclerView);
         msgRecycler=findViewById(R.id.messageRecyclerView);
         addMsgBtn=findViewById(R.id.addMsgBtn);
+        eventList.add(new Event("Cosc310 project", new Date()));
+        eventAdapter=new EventAdapter(eventList);
+        eventRecycler.setLayoutManager(new LinearLayoutManager(this));
+        eventRecycler.setAdapter(eventAdapter);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration( this, DividerItemDecoration. VERTICAL);
+        eventRecycler.addItemDecoration(dividerItemDecoration);
         addMsgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
