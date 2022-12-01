5be19d3f4c140c17e1dcd748ec7b8e139e93d8cb
Manroop22 <77290018+Manroop22@users.noreply.github.com>
2022-12-01 01:25:51 -0800

    have to continue from the saveMsg Btn ClickListener

---
+++ app/src/main/java/com/example/familyschedulingapplication/Message.java
@@ -0,0 +1,44 @@
+package com.example.familyschedulingapplication;
+
+public class Message {
+    private String title;
+    private String msgText;
+    private String NotificationType;
+
+    public Message(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMsgText() {
+        return msgText;
+    }
+
+    public void setMsgText(String msgText) {
+        this.msgText = msgText;
+    }
+
+    public String getNotificationType() {
+        return NotificationType;
+    }
+
+    public void setNotificationType(String notificationType) {
+        NotificationType = notificationType;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "title='" + title + '\'' +
+                ", msgText='" + msgText + '\'' +
+                ", NotificationType='" + NotificationType + '\'' +
+                '}';
+    }
+}
--- app/src/main/java/com/example/familyschedulingapplication/NewMessage.java
+++ app/src/main/java/com/example/familyschedulingapplication/NewMessage.java
@@ -2,15 +2,24 @@ package com.example.familyschedulingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class NewMessage extends AppCompatActivity {
     Button cancelMsgBtn;
     Button saveMsgBtn;
     ImageButton menuBtn;
+    EditText titleInput;
+    EditText msgInput;
+    CheckBox smsCheckBox;
+    CheckBox emailCheckBox;
+    CheckBox pushCheckBox;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +27,11 @@ public class NewMessage extends AppCompatActivity {
         cancelMsgBtn=findViewById(R.id.cancelMsgBtn);
         saveMsgBtn=findViewById(R.id.saveMsgBtn);
         menuBtn=findViewById(R.id.createMsgMenuBtn);
+        titleInput=findViewById(R.id.msgTitleInput);
+        msgInput=findViewById(R.id.msgMultiInput);
+        smsCheckBox=findViewById(R.id.smsCheckBox);
+        emailCheckBox=findViewById(R.id.emailCheckBox);
+        pushCheckBox=findViewById(R.id.pushCheckBox);
         cancelMsgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,7 +41,9 @@ public class NewMessage extends AppCompatActivity {
         saveMsgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // to be Implemented.
+                String title=titleInput.getText().toString();
+                String msgText=msgInput.getText().toString();
+
             }
         });
         ModalBottomSheet modalBottomSheet = new ModalBottomSheet();
